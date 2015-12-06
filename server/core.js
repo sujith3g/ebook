2b2320d0865d7d425875da17a87d03f985423582
Sujith G <sujith3g@gmail.com>
2015-12-06 15:19:15 +0530

    fixes issue#1

--- server/core.js
+++ server/core.js
@@ -7,16 +7,30 @@
 */
 //Kadira.connect('48jiwsakE94BNusZM', 'b838ad8c-5ddd-46f3-b5a3-27dd27462d19');
 Meteor.methods({
-	sample: function (keyword) {
-		apiUrl = "http://it-ebooks-api.info/v1/search/"+escape(keyword);
-		responsePage = HTTP.get(apiUrl);
-		// console.log(JSON.parse(responsePage.content));
-		return JSON.parse(responsePage.content);
-	},
-	getBook:function(bookId){
-		apiUrl = "http://it-ebooks-api.info/v1/book/"+escape(bookId);
-		responsePage = HTTP.get(apiUrl);
-		// console.log(JSON.parse(responsePage.content));
-		return JSON.parse(responsePage.content);	
-	}
+  sample: function (keyword) {
+    apiUrl = "http://it-ebooks-api.info/v1/search/"+escape(keyword);
+    responsePage = HTTP.get(apiUrl);
+    // console.log(JSON.parse(responsePage.content));
+    return JSON.parse(responsePage.content);
+  },
+  getBook:function(bookId){
+    apiUrl = "http://it-ebooks-api.info/v1/book/"+escape(bookId);
+    responsePage = HTTP.get(apiUrl);
+    // console.log(JSON.parse(responsePage.content));
+    return JSON.parse(responsePage.content);	
+  },
+  getLink: function(url){
+    var response = HTTP.get(url,{
+      npmRequestOptions: {
+        headers: {
+          'referer': 'http://it-ebooks.info/'
+        },
+        followRedirect: false
+      }
+    });
+    if(response.statusCode === 302){
+        //console.log(response.headers.location);
+        return response.headers.location;
+      }
+  }
 });
