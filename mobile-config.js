c25d63a4683e115e339c52a096722d8bf6184c6e
Sujith G <sujith3g@gmail.com>
2015-12-21 00:12:04 +0530

    Android app beta version, cordova specific code

---
+++ mobile-config.js
@@ -0,0 +1,8 @@
+App.info({
+  id: 'com.meteor.ebook',
+  name: 'Free Ebooks',
+  description: 'Get Free Tech Ebooks',
+  author: 'Sujith G',
+  email: 'sujith3g@gmail.com',
+  website: 'http://ebook.meteor.com'
+});
