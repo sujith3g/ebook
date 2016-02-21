c99fdffa96ebf8d86657241f5c4bd2bdb31652dd
Sujith G <sujith3g@gmail.com>
2016-02-21 23:31:35 +0530

    Version updated

--- mobile-config.js
+++ mobile-config.js
@@ -3,6 +3,7 @@ App.info({
   name: 'Free Ebooks',
   description: 'Get Free Tech Ebooks',
   author: 'Sujith G',
+  version: '0.0.2',
   email: 'sujith3g@gmail.com',
   website: 'http://ebook.meteor.com'
 });
